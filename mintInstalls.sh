9314e80a16b482ab13bd2547e8e1110dbf204b29
Heidi Rodenhizer <hgr7@nau.edu>
2021-01-09 17:30:38 -0700

    add QGIS, CloudCompare, and citation managers

--- mintInstalls.sh
+++ mintInstalls.sh
@@ -9,6 +9,10 @@ set -e
 
 sudo -s -- <<EOF
 # enter password
+
+# this one requires input for the version
+flatpak install org.qgis.qgis -y
+
 apt update
 apt upgrade -y
 apt dist-upgrade -y
@@ -18,9 +22,13 @@ apt autoclean -y
 # Optional software:
 apt install -y vlc
 apt install -y clementine
+apt install -y spotify-client
 apt install -y chromium
 apt install -y htop
 apt install r-base -y
+flatpak install flathub com.elsevier.MendeleyDesktop -y
+flatpak install flathub org.zotero.Zotero -y
+flatpak install org.cloudcompare.CloudCompare -y
 
 EOF
 
