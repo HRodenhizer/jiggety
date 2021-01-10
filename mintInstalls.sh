820f99b05dc22ed4d4c4509cb32a67350e454de1
Heidi Rodenhizer <hgr7@nau.edu>
2021-01-09 20:17:32 -0700

    Whitespace

--- mintInstalls.sh
+++ mintInstalls.sh
@@ -8,27 +8,26 @@ set -e
 # apt install installs em. `-y` makes it so you don't need to confirm before each install.
 
 sudo -s -- <<EOF
-# enter password
-
-# this one requires input for the version
-flatpak install org.qgis.qgis -y
-
-apt update
-apt upgrade -y
-apt dist-upgrade -y
-apt autoremove
-apt autoclean -y
-
-# Optional software:
-apt install -y vlc
-apt install -y clementine
-apt install -y spotify-client
-apt install -y chromium
-apt install -y htop
-apt install r-base -y
-flatpak install flathub com.elsevier.MendeleyDesktop -y
-flatpak install flathub org.zotero.Zotero -y
-flatpak install org.cloudcompare.CloudCompare -y
-
+    # enter password
+    
+    # this one requires input for the version
+    flatpak install org.qgis.qgis -y
+    
+    apt update
+    apt upgrade -y
+    apt dist-upgrade -y
+    apt autoremove
+    apt autoclean -y
+    
+    # Optional software:
+    apt install -y vlc
+    apt install -y clementine
+    apt install -y spotify-client
+    apt install -y chromium
+    apt install -y htop
+    apt install r-base -y
+    flatpak install flathub com.elsevier.MendeleyDesktop -y
+    flatpak install flathub org.zotero.Zotero -y
+    flatpak install org.cloudcompare.CloudCompare -y
 EOF
 
