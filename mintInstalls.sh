a508122f87b6129cab625ee3da6bef76160f9a2f
Heidi Rodenhizer <hgr7@nau.edu>
2021-01-12 19:27:13 -0700

    Adds R, geospatial dependencies, and openconnect

--- mintInstalls.sh
+++ mintInstalls.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
 set -e
-# TODO: Install R and relevant packages (ggplot2, dplyr, etc)
-
 # This script is the place to put all software installation commands
 # apt search lets you search for programs
 # apt install installs em. `-y` makes it so you don't need to confirm before each install.
 
 sudo -s -- <<EOF
     # enter password
-    
-    # this one requires input for the version
-    flatpak install org.qgis.qgis -y
-    
+
     apt update
     apt upgrade -y
     apt dist-upgrade -y
     apt autoremove
     apt autoclean -y
     
+    # this one requires input for the version
+    flatpak install org.qgis.qgis -y
+
+    # install R
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+    add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+    apt update
+    apt install r-base -y
+
+    # R package dependencies
+    # for rgeos
+    apt install libgeos-dev -y
+    # for sf
+    apt install libudunits2-dev -y
+    apt install libgdal-dev -y
+    # for tidyverse
+    apt install libcurl4-openssl-dev libssl-dev libxml2-dev -y
+
     # Optional software:
+    apt install openconnect -y
     apt install -y vlc
     apt install -y clementine
     apt install -y spotify-client
     apt install -y chromium
     apt install -y htop
-    apt install r-base -y
     flatpak install flathub com.elsevier.MendeleyDesktop -y
     flatpak install flathub org.zotero.Zotero -y
     flatpak install org.cloudcompare.CloudCompare -y
